e1e6554c15c0ec57ad2bdb5d7c0cf32a3efcac4e
marchuanv <marchuanv@gmail.com>
2020-07-15 05:42:29 +0200

    config

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -3,20 +3,23 @@ const delegate = require("component.delegate");
 const logging = require("logging");
 logging.config.add("Request Handler Route");
 module.exports = { 
+    routes: [],
     handle: (callingModule, { port, path }) => {
-        const thisModule = `component.request.handler.route.${path.replace(/\//g,"")}.${port}`;
+        const thisModule = `component.request.handler.route.${port}`;
+        module.exports.routes.push({ module: callingModule, port, path });
         delegate.register(thisModule, async (request) => {
             let results = { headers: {}, statusCode: -1, statusMessage: "" };
-            if (request.path === path && request.port === port) {
-                return await delegate.call(callingModule, request);
+            const route = module.exports.routes.find(r => r.path === request.path && r.port === request.port);
+            if (route) {
+                return await delegate.call(route.module, request);
             } else {
                 const message = "Not Found";
                 results.statusCode = 404;
                 results.statusMessage = message;
                 results.headers = { "Content-Type":"text/plain", "Content-Length": Buffer.byteLength(message) };
                 results.data = message;
+                return results;
             }
-            return results;
         });
         requestHandler.handle(thisModule, { port });
     }
