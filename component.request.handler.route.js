2748f013f3110947f670e4c29f4c3e55e5c802e3
marchuanv <marchuanv@gmail.com>
2020-07-22 15:18:24 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -10,18 +10,15 @@ module.exports = {
         module.exports.routes.push({ publicPort: options.publicPort, path: options.path });
         delegate.register(`component.request.handler.route`, options.publicPort, async (request) => {
             const routes = module.exports.routes.filter(r => r.publicPort === options.publicPort && !r.locked);
-            if (intervalId){
-                clearInterval(intervalId);
-            }
-            intervalId = setInterval(()=>{
-                routes.forEach(r => r.locked = false);
-                clearInterval(intervalId);
-            },200);
             routes.forEach(r => r.locked = true);
             if (routes.length > 0){
                 const foundRoute = routes.find(r => r.path === request.path);
                 if (foundRoute){
-                    const result = await delegate.call( { context: `component.request.handler.deferred`, name: foundRoute.path }, request);
+                    const name = `${foundRoute.publicPort}${foundRoute.path}`;
+                    const result = await delegate.call( { context: `component.request.handler.deferred`, name }, request);
+                    setTimeout(()=>{
+                        routes.forEach(r => r.locked = false);
+                    },200);
                     return result;
                 } else {
                     const statusMessage = "Not Found";
