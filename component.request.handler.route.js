6d9637196802b9f0581aa8b8344fdf14a9e520bf
marchuanv <marchuanv@gmail.com>
2020-07-22 12:55:30 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -3,12 +3,9 @@ const delegate = require("component.delegate");
 const logging = require("logging");
 logging.config.add("Request Handler Route");
 module.exports = {
-    // routes: [],
     handle: (options) => {
         requestHandler.handle(options);
-        // module.exports.routes.push({ publicPort: options.publicPort, path: options.path });
         delegate.register(`component.request.handler.route.${options.publicPort}`, "route", async (request) => {
-            // const foundRoute = module.exports.routes.find(r => r.publicPort === request.publicPort && r.path === request.path);
             if (options.path === request.path){
                 return await delegate.call( { context: `component.request.handler.deferred.${foundRoute.path}` }, request);
             } else {
