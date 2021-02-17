d69c985a83940a00843434bc83a2efd71f80d9e9
marchuanv <marchuanv@gmail.com>
2021-02-17 03:33:11 +0200

    fixed routing

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -7,23 +7,25 @@ const routes = [];
 module.exports = {
     handle: (options) => {
         requestHandler.handle({ host: options.host, port: options.port });
-        routes.push( { host: options.host, port: options.port, path: options.path } );
+        routes.push( { host: options.host, port: options.port, path: options.path, requests: [] } );
         const routeName = `${options.port}${options.path}`;
         delegate.register(`component.request.handler.route`, routeName, async (request) => {
-            if ((await delegate.callbackCount( { context: `component.request.handler.route` })) === 1) {
-                const foundRoute = routes.find(r => r.port === request.port && r.path === request.path);
-                if (foundRoute){
+            const foundRoute = routes.find(r => r.port === request.port && r.path === request.path);
+            if (foundRoute) {
+                const isHandled = foundRoute.requests.find(id => id === request.id);
+                if (!isHandled){
+                    foundRoute.requests.push(request.id);
                     const name = `${foundRoute.port}${foundRoute.path}`;
                     return await delegate.call( { context: `component.request.handler.deferred`, name }, request );
-                } else {
-                    const statusMessage = "Not Found";
-                    return { 
-                        headers: { "Content-Type":"text/plain" },
-                        statusCode: 404,
-                        statusMessage,
-                        data: statusMessage
-                    };
                 }
+            } else {
+                const statusMessage = "Not Found";
+                return { 
+                    headers: { "Content-Type":"text/plain" },
+                    statusCode: 404,
+                    statusMessage,
+                    data: statusMessage
+                };
             }
         });
     }
--- test.js
+++ test.js
@@ -1,8 +1,13 @@
 const requestHandlerRoute = require("./component.request.handler.route.js");
+const request = require("component.request");
 const delegate = require("component.delegate");
 (async()=>{ 
   
     delegate.register("component.request.handler.deferred", "3000/test1", () => {
+        return { statusCode: 200, statusMessage: "Success", headers: {}, data: "test passed" };
+    });
+
+    delegate.register("component.request.handler.deferred", "3000/test2", () => {
         return { statusCode: 200, statusMessage: "Success", headers: {}, data: "test passed" };
     });
   
@@ -16,6 +21,33 @@ const delegate = require("component.delegate");
         port: 3000,
         path: "/test2"
     });
+
+    let results = await request.send({ 
+        host: "localhost",
+        port: 3000,
+        path: "/test1",
+        method: "GET",
+        headers: {}, 
+        data: "",
+        retryCount: 1
+    });
+    if (results.statusCode !== 200){
+        throw "routing for test 01 failed";
+    }
+
+    results = await request.send({ 
+        host: "localhost",
+        port: 3000,
+        path: "/test2",
+        method: "GET",
+        headers: {}, 
+        data: "",
+        retryCount: 1
+    });
+    if (results.statusCode !== 200){
+        throw "routing for test 02 failed";
+    }
+
 ;
 })().catch((err)=>{
     console.error(err);
