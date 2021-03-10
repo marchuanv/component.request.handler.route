c8d1728bcaaf2833029b19cc3e6f0e0c37a28568
marchuanv <marchuanv@gmail.com>
2021-03-10 02:56:33 +0200

    fix

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -1,22 +1,19 @@
 const component = require("component");
 const delegate = require("component.delegate");
-const routes = [];
+const { routes } = require("./package.json");
 
-module.exports = {
-    handle: async (options) => {
-        const { componentRequestHandler } = await component.require("component.request.handler", {gitUsername: "marchuanv"});
-        const { componentLogging } = await component.require("component.logging", {gitUsername: "marchuanv"});
-
-        await componentRequestHandler.handle({ host: options.host, port: options.port });
-        const routeName = `${options.port}${options.path}`;
-        const existingRouteIndex = routes.findIndex(r => r.host == options.host && r.port === options.port && r.path === options.path);
-        if (existingRouteIndex > -1){
-            routes.splice(existingRouteIndex,1);
-        }
-        routes.push( { host: options.host, port: options.port, path: options.path, requests: [] } );
-        delegate.register({ context: "component.request.handler.route", name: routeName }, async (request) => {
+component.require("component.request.handler", { gitUsername: "marchuanv" }).then(({ hostname, port }) => {
+    for(const route of routes){
+        route.host = hostname;
+        route.port = port;
+    };
+    component.require("component.logging", {gitUsername: "marchuanv"}).then(({ componentLogging }) => {
+        delegate.register({ context: "component.request.handler.route", name: port }, async (request) => {
             const foundRoute = routes.find(r => r.port === request.port && r.path === request.path);
             if (foundRoute) {
+                if (!foundRoute.requests){
+                    foundRoute.requests = [];
+                }
                 const isHandled = foundRoute.requests.find(id => id === request.id);
                 if (!isHandled){
                     foundRoute.requests.push(request.id);
@@ -33,5 +30,6 @@ module.exports = {
                 };
             }
         });
-    }
-};
+    });
+});
+module.exports = {};
--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-const requestHandlerRoute = require("./component.request.handler.route.js");
 const delegate = require("component.delegate");
 const com = require("component");
 (async()=>{ 
+    
+    require("./component.request.handler.route.js");
   
     delegate.register( { context: "component.request.handler.deferred", name: "3000/test1" }, () => {
         return { statusCode: 200, statusMessage: "Success", headers: {}, data: "test passed" };
@@ -9,22 +10,6 @@ const com = require("component");
 
     delegate.register( { context: "component.request.handler.deferred", name: "3000/test2" }, () => {
         return { statusCode: 200, statusMessage: "Success", headers: {}, data: "test passed" };
-    });
-  
-    await requestHandlerRoute.handle({
-        host: "localhost",
-        port: 3000,
-        path: "/test1"
-    });
-    await requestHandlerRoute.handle({
-        host: "localhost",
-        port: 3000,
-        path: "/test2"
-    });
-    await requestHandlerRoute.handle({
-        host: "localhost",
-        port: 3000,
-        path: "/test2"
     });
 
     const { componentRequest } = await com.require("component.request", { gitUsername: "marchuanv" });
