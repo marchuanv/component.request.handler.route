38a9a9f85d0c6e564ebac52e8da85937ec88a2ce
marchuanv <marchuanv@gmail.com>
2021-03-12 03:46:17 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -1,34 +1,32 @@
 const component = require("component");
 const { routes } = require("./package.json");
 
-component.load({ moduleName: "component.logging", gitUsername: "marchuanv" }).then(({ logging }) => {
-    component.load({ moduleName: "component.request.handler", gitUsername: "marchuanv" }).then(({ config }) => {
-        for(const route of routes){
-            route.host = config.hostname;
-            route.port = config.port;
-        };
-        await component.events.register({ componentModule: module, componentParentModuleName: "component.request.handler.deferred" });
-        component.events.requestHandlerRoute.register({ name: port }, async (request) => {
-            const foundRoute = routes.find(r => r.port === request.port && r.path === request.path);
-            if (foundRoute) {
-                if (!foundRoute.requests){
-                    foundRoute.requests = [];
-                }
-                if (!foundRoute.requests.find(id => id === request.id)){
-                    foundRoute.requests.push(request.id);
-                    const name = `${foundRoute.port}${foundRoute.path}`;
-                    logging.write("component.request.handler.route", `calling callback for route ${foundRoute.path}` );
-                    return await component.events.requestHandlerRoute.call( { name }, request );
-                }
-            } else {
-                const statusMessage = "Not Found";
-                return { 
-                    headers: { "Content-Type":"text/plain" },
-                    statusCode: 404,
-                    statusMessage,
-                    data: statusMessage
-                };
+component.load({ moduleName: "component.request.handler", gitUsername: "marchuanv" }).then(({ logging, config }) => {
+    for(const route of routes){
+        route.host = config.requestHandler.hostname;
+        route.port = config.requestHandler.port;
+    };
+    await component.events.register({ componentModule: module, componentParentModuleName: "component.request.handler.deferred" });
+    component.events.requestHandlerRoute.register({ name: port }, async (request) => {
+        const foundRoute = routes.find(r => r.port === request.port && r.path === request.path);
+        if (foundRoute) {
+            if (!foundRoute.requests){
+                foundRoute.requests = [];
             }
-        });
+            if (!foundRoute.requests.find(id => id === request.id)){
+                foundRoute.requests.push(request.id);
+                const name = `${foundRoute.port}${foundRoute.path}`;
+                logging.write("component.request.handler.route", `calling callback for route ${foundRoute.path}` );
+                return await component.events.requestHandlerRoute.call( { name }, request );
+            }
+        } else {
+            const statusMessage = "Not Found";
+            return { 
+                headers: { "Content-Type":"text/plain" },
+                statusCode: 404,
+                statusMessage,
+                data: statusMessage
+            };
+        }
     });
 });
