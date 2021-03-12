bd8a65747f943d1c457639072311a7484b09b9d5
marchuanv <marchuanv@gmail.com>
2021-03-12 03:58:25 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -1,13 +1,13 @@
 const component = require("component");
 const { routes } = require("./package.json");
+component.events.register({ componentModule: module, componentParentModuleName: "component.request.handler.deferred" });
 
-component.load({ moduleName: "component.request.handler", gitUsername: "marchuanv" }).then( async ({ logging, config }) => {
+component.load({ moduleName: "component.request.handler" }).then( async ({ logging, config }) => {
     for(const route of routes){
         route.host = config.requestHandler.hostname;
         route.port = config.requestHandler.port;
     };
-    await component.events.register({ componentModule: module, componentParentModuleName: "component.request.handler.deferred" });
-    component.events.requestHandlerRoute.register({ name: port }, async (request) => {
+    component.events.requestHandlerRoute.subscribe({ name: config.requestHandler.port }, async (request) => {
         const foundRoute = routes.find(r => r.port === request.port && r.path === request.path);
         if (foundRoute) {
             if (!foundRoute.requests){
@@ -17,7 +17,7 @@ component.load({ moduleName: "component.request.handler", gitUsername: "marchuan
                 foundRoute.requests.push(request.id);
                 const name = `${foundRoute.port}${foundRoute.path}`;
                 logging.write("component.request.handler.route", `calling callback for route ${foundRoute.path}` );
-                return await component.events.requestHandlerRoute.call( { name }, request );
+                return await component.events.requestHandlerRoute.publish( { name }, request );
             }
         } else {
             const statusMessage = "Not Found";
