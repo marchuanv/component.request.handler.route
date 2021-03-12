5fbba5884bf9c998d1d550aa418d6f2202697926
marchuanv <marchuanv@gmail.com>
2021-03-12 03:37:06 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -1,25 +1,24 @@
 const component = require("component");
-const delegate = require("component.delegate");
 const { routes } = require("./package.json");
 
-component.require("component.request.handler", { gitUsername: "marchuanv" }).then(({ hostname, port }) => {
-    for(const route of routes){
-        route.host = hostname;
-        route.port = port;
-    };
-    component.require("component.logging", {gitUsername: "marchuanv"}).then(({ componentLogging }) => {
-        delegate.register({ context: "component.request.handler.route", name: port }, async (request) => {
+component.load({ moduleName: "component.logging", gitUsername: "marchuanv" }).then(({ logging }) => {
+    component.load({ moduleName: "component.request.handler", gitUsername: "marchuanv" }).then(({ config }) => {
+        for(const route of routes){
+            route.host = config.hostname;
+            route.port = config.port;
+        };
+        await component.events.register({ componentModule: module, componentParentModuleName: "component.request.handler.deferred" });
+        component.events.requestHandlerRoute.register({ name: port }, async (request) => {
             const foundRoute = routes.find(r => r.port === request.port && r.path === request.path);
             if (foundRoute) {
                 if (!foundRoute.requests){
                     foundRoute.requests = [];
                 }
-                const isHandled = foundRoute.requests.find(id => id === request.id);
-                if (!isHandled){
+                if (!foundRoute.requests.find(id => id === request.id)){
                     foundRoute.requests.push(request.id);
                     const name = `${foundRoute.port}${foundRoute.path}`;
-                    componentLogging.write("component.request.handler.route", `calling callback for route ${foundRoute.path}` );
-                    return await delegate.call( { context: `component.request.handler.deferred`, name }, request );
+                    logging.write("component.request.handler.route", `calling callback for route ${foundRoute.path}` );
+                    return await component.events.requestHandlerRoute.call( { name }, request );
                 }
             } else {
                 const statusMessage = "Not Found";
@@ -33,4 +32,3 @@ component.require("component.request.handler", { gitUsername: "marchuanv" }).the
         });
     });
 });
-module.exports = {};
