8ce4d29a1cf93062d542727d9b4890378a548f10
marchuanv <marchuanv@gmail.com>
2021-03-23 04:30:55 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -1,19 +1,13 @@
 const component = require("component");
-const { routes } = require("./package.json");
+const { routes, hostname, port } = require("./package.json");
 component.register({ componentPackagePath: `${__dirname}/package.json` }).then( async ({ requestHandlerRoute }) => {
-    const { config } = await component.load({ moduleName: "component.request.handler" });
-    for(const route of routes) {
-        route.host = config.requestHandler.hostname;
-        route.port = config.requestHandler.port;
-    };
-    requestHandlerRoute.subscribe({ name: config.requestHandler.port }, async (request) => {
+    requestHandlerRoute.subscribe({ name: port }, async (request) => {
         const foundRoute = routes.find(r => r.port === request.port && r.path === request.path);
         if (foundRoute) {
             if (!foundRoute.requests){
                 foundRoute.requests = [];
             }
             if (!foundRoute.requests.find(id => id === request.id)){
-                module.exports = foundRoute;
                 foundRoute.requests.push(request.id);
                 const name = `${foundRoute.port}${foundRoute.path}`;
                 requestHandlerRoute.log(`calling callback for route ${foundRoute.path}` );
@@ -30,4 +24,8 @@ component.register({ componentPackagePath: `${__dirname}/package.json` }).then( 
         }
     });
 });
-
+module.exports = { 
+    addPath: ({ path }) => {
+        routes.push({ host: hostname, port, path });
+    }
+};
