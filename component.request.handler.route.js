482036f1d35ca4628c2342c9e11c32c29d1b273c
marchuanv <marchuanv@gmail.com>
2021-03-30 03:08:13 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -1,16 +1,16 @@
 const component = require("component");
 component.register(module).then( async ({ requestHandlerRoute }) => {
-    requestHandlerRoute.subscribe({ name: requestHandlerRoute.port }, async (request) => {
-        const foundRoute = requestHandlerRoute.routes.find(r => r.path === request.path);
+    const { channel, routes } = requestHandlerRoute;
+    requestHandlerRoute.subscribe({ channel }, async ({ requestId, path }) => {
+        const foundRoute = routes.find(r => r.path === path);
         if (foundRoute) {
             if (!foundRoute.requests){
                 foundRoute.requests = [];
             }
-            if (!foundRoute.requests.find(id => id === request.id)){
-                foundRoute.requests.push(request.id);
-                const name = `${requestHandlerRoute.port}${foundRoute.path}`;
+            if (!foundRoute.requests.find(id => id === requestId)){
+                foundRoute.requests.push(requestId);
                 await requestHandlerRoute.log(`calling callback for route ${foundRoute.path}` );
-                return await requestHandlerRoute.publish( { name }, request );
+                return await requestHandlerRoute.publish( { channel }, request );
             }
         } else {
             const statusMessage = "Not Found";
