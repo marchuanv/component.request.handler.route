49a9d9ab16a8089922fada8a3a61cd2ee742f386
marchuanv <marchuanv@gmail.com>
2021-04-09 23:42:05 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -1,7 +1,7 @@
 const component = require("component");
 component.load(module).then( async ({ requestHandlerRoute }) => {
     const { channel, routes } = requestHandlerRoute.config;
-    requestHandlerRoute.subscribe({ channel }, async ({ headers, session, data }) => {
+    requestHandlerRoute.subscribe({ channel }, async ({ headers, session, data, path, requestId }) => {
         const foundRoute = routes.find(r => r.path === path);
         if (foundRoute) {
             if (!foundRoute.requests){
