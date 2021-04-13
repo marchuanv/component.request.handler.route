b45bb87995196cf897074925f677dc1e0410ebc8
marchuanv <marchuanv@gmail.com>
2021-04-13 15:42:36 +0200

    refactor

--- component.request.handler.route.js
+++ component.request.handler.route.js
@@ -11,18 +11,34 @@ component.load(module).then( async ({ requestHandlerRoute }) => {
         if (foundRoute) {
             if (foundRoute.isRegisterRoute) {
                 const { path, hashedPassphrase, hashedPassphraseSalt } = utils.getJSONObject(request.data) || {};
-                if (path !== undefined) { //request for a new route
+                if (path) { //request for a new route
                     const newRoute = new Route(path);
                     if (hashedPassphrase && hashedPassphraseSalt) {
                         newRoute.secure(hashedPassphrase, hashedPassphraseSalt);
                     }
                     registeredRoutes.push(newRoute);
+                    const statusMessage = `${path} registered`;
+                    return { 
+                        headers: { "Content-Type":"text/plain" },
+                        statusCode: 200,
+                        statusMessage,
+                        data: statusMessage
+                    };
+                } else {
+                    const statusMessage = `failed to register route, no URL path was specified.`;
+                    return { 
+                        headers: { "Content-Type":"text/plain" },
+                        statusCode: 500,
+                        statusMessage,
+                        data: statusMessage
+                    };
                 }
-            }
-            if (!foundRoute.hasRequestId(request.requestId)){
-                foundRoute.addRequestId(request.requestId);
-                await requestHandlerRoute.log(`calling callback for route ${foundRoute.path}` );
-                return await requestHandlerRoute.publish({ session, request, route: foundRoute } );
+            } else {
+                if (!foundRoute.hasRequestId(request.requestId)){
+                    foundRoute.addRequestId(request.requestId);
+                    await requestHandlerRoute.log(`calling callback for route ${foundRoute.path}` );
+                    return await requestHandlerRoute.publish({ session, request, route: foundRoute } );
+                }
             }
         } else {
             const statusMessage = "Not Found";
